646fc1121e219d572fdb0a3d7312b49241e3f5c4
yowcow <yowcow@cpan.org>
2016-09-13 05:03:40 +0000

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 4a4fe2b..e577416 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Log-Dispatch-CronoDir
    
     {{$NEXT}}
    
    +0.06 2016-09-13T05:03:26Z
    +
         - Make "permission" optional.
           When not specified, creating directory's permissions are based on current umask.

--- lib/Log/Dispatch/CronoDir.pm
+++ lib/Log/Dispatch/CronoDir.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use parent qw(Log::Dispatch::Output);
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 
 use File::Path qw(make_path);
 use Params::Validate qw(validate SCALAR BOOLEAN);
